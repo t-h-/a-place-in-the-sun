bad433a0c919f9e654252e0caf6801a48c9d02a6
tomislav.hasan <tomislav.hasan@degura.de>
2022-01-31 16:22:43 +0100

    initial commit

---
+++ src/views/Sun.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sun">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Lets see where the Sun is!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import SunMap from '@/components/SunMap.vue';
+
+export default {
+  name: 'Sun',
+  components: {
+    SunMap,
+  },
+};
+</script>
