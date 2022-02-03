635b6bd590022bbae994009fccc0288fe64ce0b4
t-h- <tomi.hasan@gmail.com>
2022-02-03 19:31:05 +0100

    clarifies how to dynamically interact with heatmap

--- src/components/SunMap.vue
+++ src/components/SunMap.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="sunmap">
     <h1>{{ msg }}</h1>
-      <l-map style="height: 600px" :zoom="zoom" :center="center">
+      <l-map ref="leafletMap" @moveend="recalc" style="height: 600px" :zoom="zoom" :center="center">
       <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-      <LHeatmap :latLng=latLngArray :radius=radius :blur=blur :gradient=gradient :max=max>
-      </LHeatmap>
+      <LHeatmap ref="heatmapLayer" :latLng=latLngArray :radius=radius :blur=blur :gradient=gradient :max=max></LHeatmap>
       </l-map>
   </div>
 </template>
@@ -32,18 +31,28 @@ export default {
       zoom: 11,
       center: [50.50, 30.5],
       latLngArray: [
-        [50.50, 30.50, 4],
+        [50.50, 30.50, 40],
         [50.50, 30.51, 80.80],
         [50.50, 30.52, 80.80],
         [50.50, 30.53, 80.80],
-        [50.50, 30.64, 80.80],
+        [50.50, 30.64, 50],
         [50.50, 30.74, 80.80],
       ],
       max: null,
-      radius: 50,
+      radius: 100,
       blur: 30,
       gradient: { 0.1: 'green', 0.5: 'yellow', 1.0: 'orange' },
     };
+  },
+  methods: {
+    recalc(mapObject) {
+      console.log('recalc');
+      console.log(mapObject);
+      const bnds = this.$refs.leafletMap.mapObject.getBounds();
+      console.log(bnds);
+      this.latLngArray.push([50.45, 30.74, 80.80]);
+      this.$refs.heatmapLayer.addLatLng([50.55, 30.74, 80.80]);
+    },
   },
 };
 </script>
